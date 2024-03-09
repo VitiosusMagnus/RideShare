87cc135ce4132faf4ac16099ab4cfaa1034d9c4a
fokal <furkancanokal1@gmail.com>
2024-03-09 22:15:33 +0300

    ADD: user controller

---
+++ src/main/java/com/fokal/rideshare/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.fokal.rideshare.controller;
+
+import com.fokal.rideshare.dto.UserCreateRequest;
+import com.fokal.rideshare.dto.UserGetAllResponse;
+import com.fokal.rideshare.dto.UserGetResponse;
+import com.fokal.rideshare.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("")
+    public List<UserGetAllResponse> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserGetResponse getUserById(Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("")
+    public UserGetResponse createUser(@RequestBody UserCreateRequest userCreateRequest) {
+        return userService.createUser(userCreateRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
