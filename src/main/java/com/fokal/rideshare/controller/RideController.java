990385bbcdccd2c11b4c2075558e7131805328fa
fokal <furkancanokal1@gmail.com>
2024-03-09 22:15:25 +0300

    ADD: ride controller

---
+++ src/main/java/com/fokal/rideshare/controller/RideController.java
@@ -0,0 +1,18 @@
+package com.fokal.rideshare.controller;
+
+import com.fokal.rideshare.service.RideService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/ride")
+public class RideController {
+
+        private final RideService rideService;
+
+}
