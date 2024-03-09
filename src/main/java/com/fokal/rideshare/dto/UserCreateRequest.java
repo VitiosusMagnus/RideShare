c1d49d8b2bc348bab0baa9bac5b68f7b4ae431ff
fokal <furkancanokal1@gmail.com>
2024-03-09 22:14:51 +0300

    ADD: UserCreateRequest

---
+++ src/main/java/com/fokal/rideshare/dto/UserCreateRequest.java
@@ -0,0 +1,15 @@
+package com.fokal.rideshare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateRequest {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+}
