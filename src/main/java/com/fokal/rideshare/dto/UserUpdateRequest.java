b6fbcaf4eaf49d2a1f3767da2a381e4b85f67687
fokal <furkancanokal1@gmail.com>
2024-03-10 18:53:40 +0300

    ADD: userUpdate

---
+++ src/main/java/com/fokal/rideshare/dto/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.fokal.rideshare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+}
